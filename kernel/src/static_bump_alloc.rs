904cb6111f9d56a63173e9decc65d5c90a0059e3
harikrishnan94 <harikrishnan.prabakaran@gmail.com>
2023-01-02 16:22:55 +0530

    Fix Static Bump Allocator

--- kernel/src/static_bump_alloc.rs
+++ kernel/src/static_bump_alloc.rs
@@ -3,23 +3,17 @@ use core::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-const MEMORY_REGION_LEN: usize = 8192;
+const DEF_MEMORY_REGION_LEN: usize = 8192;
 
-/// Initialized by ASM
-#[no_mangle]
-static MEMORY_REGION_BASE: usize = 0;
-
-pub struct BumpAllocator {
-    memory_region_len: usize,
-    memory_region_base: usize,
+pub struct BumpAllocator<const MEMORY_REGION_LEN: usize> {
+    memory_region: [u8; MEMORY_REGION_LEN],
     memory_region_used: AtomicUsize,
 }
 
-impl BumpAllocator {
-    fn default() -> Self {
+impl<const MEMORY_REGION_LEN: usize> BumpAllocator<MEMORY_REGION_LEN> {
+    fn new() -> Self {
         BumpAllocator {
-            memory_region_base: MEMORY_REGION_BASE,
-            memory_region_len: MEMORY_REGION_LEN,
+            memory_region: [0; MEMORY_REGION_LEN],
             memory_region_used: AtomicUsize::new(0),
         }
     }
@@ -29,14 +23,14 @@ impl BumpAllocator {
             let used = self.memory_region_used.load(Ordering::Acquire);
             let new = used + size_of::<T>();
 
-            if new < self.memory_region_len {
+            if new < MEMORY_REGION_LEN {
                 if self
                     .memory_region_used
                     .compare_exchange_weak(used, new, Ordering::Release, Ordering::Relaxed)
                     .is_ok()
                 {
                     unsafe {
-                        let ptr = ((self.memory_region_base + used) as *mut MaybeUninit<T>)
+                        let ptr = ((self.memory_region_base() + used) as *mut MaybeUninit<T>)
                             .as_mut()
                             .unwrap();
                         ptr.write(val);
@@ -48,8 +42,18 @@ impl BumpAllocator {
             }
         }
     }
+
+    fn memory_region_base(&self) -> usize {
+        self.memory_region.as_ptr() as usize
+    }
+}
+
+impl<const MEMORY_REGION_LEN: usize> Default for BumpAllocator<MEMORY_REGION_LEN> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 lazy_static! {
-    pub static ref BUMP_ALLOCATOR: BumpAllocator = BumpAllocator::default();
+    pub static ref BUMP_ALLOCATOR: BumpAllocator<DEF_MEMORY_REGION_LEN> = BumpAllocator::default();
 }
