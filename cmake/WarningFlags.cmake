73c646faae45c61c632ad80e430ff1ef3b5a8bed
Harikrishnan <harikrishnan.prabakaran@gmail.com>
2023-05-02 04:21:09 +0000

    Make WarningFlags.cmake generic to both clang and gcc

--- cmake/WarningFlags.cmake
+++ cmake/WarningFlags.cmake
@@ -1,10 +1,7 @@
 option(ENABLE_WERROR "Enable Werror." ON)
 
 function(add_warning_flags)
-    add_compile_options(-Wall -Wextra -Wno-unused-function)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        add_compile_options(-Wunused-const-variable=1)
-    endif()
+    add_compile_options(-Wall -Wextra -Wno-unused-function -Wunused-const-variable)
 
     if(ENABLE_WERROR)
         add_compile_options(-Werror)
